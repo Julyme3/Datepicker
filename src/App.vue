577ce5b15a0902fb782227f4e3a055c37c502d6a
Julyme3 <jg0311@bk.ru>
2022-06-13 20:02:05 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <DatePicker />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import DatePicker from './components/DatePicker.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    DatePicker,
   },
 });
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: system, -apple-system, BlinkMacSystemFont,'Segoe UI','Segoe WP', Roboto, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/DatePicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="datepicker">
+    <form id="datepicker-form">
+      <label for="date" class="datepicker-label">Select the date
+        <input type="text" id="date" class="datepicker-input" />
+      </label>
+      <button class="datepucker-submit">submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'DatePicker',
+});
+</script>
+
+<style scoped lang="less"></style>
