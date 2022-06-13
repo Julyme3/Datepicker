f8a32ec33a1c0d61d459db90b2e72c671991437b
Julyme3 <jg0311@bk.ru>
2022-06-13 20:23:19 +0300

    style for datepicker container

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="datepicker">
-    <form id="datepicker-form">
-      <label for="date" class="datepicker-label">Select the date
-        <input type="text" id="date" class="datepicker-input" />
-      </label>
-      <button class="datepucker-submit">submit</button>
-    </form>
+    <div class="datepicker-container">
+      <form id="datepicker-form">
+        <label for="date" class="datepicker-label">Select the date
+          <input type="text" id="date" class="datepicker-input" />
+        </label>
+        <button class="datepicker-submit">submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -17,4 +19,46 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+
+.datepicker {
+  &-container {
+    width: 50%;
+    margin: 50px auto;
+    padding: 10px;
+    width: 50%;
+    border-radius: 3px;
+    box-shadow: 0 2px 8px rgba(0,0,0, 0.26);
+    text-align: left;
+  }
+
+  &-label {
+    display: block;
+    margin-bottom: 16px;
+    color: #555;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  &-input {
+    width: 100%;
+    height: 24px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 3px;
+    padding: 2px;
+    margin-top: 4px;
+  }
+
+  &-submit {
+    border: none;
+    border-radius: 3px;
+    background-color: #38ACEC;
+    padding: 10px 14px;
+    color: #FEFCFF;
+    font-weight: bold;
+    text-transform: capitalize;
+    cursor: pointer;
+  }
+}
+</style>
